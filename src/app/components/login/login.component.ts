8685002e7aaf46d54fd43e8d715cca633a8ffd8a
BayerTobias <tbayer2@gmx.de>
2024-01-31 21:06:34 +0100

    login function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,12 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  email: String = '';
+  username: String = '';
   password: String = '';
 
   constructor() {}
 
-  async login() {
-    // login code
+  async loginWithUsernameAndPassword() {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    var raw = JSON.stringify({
+      username: this.username,
+      password: 'Test123',
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    try {
+      const resp = await fetch('http://127.0.0.1:8000/login/', requestOptions);
+      const json = await resp.json();
+      localStorage.setItem('token', json.token);
+      console.log('Token set:', json.token);
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
