2888d39ced730c9b37929f95965e53710c97c748
BayerTobias <tbayer2@gmx.de>
2024-02-03 16:17:08 +0100

    create todo & delete todo

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -9,12 +9,34 @@ import { AuthService } from 'src/app/services/auth.service';
 export class AllTodosComponent {
   allTodos: any = [];
 
+  title: string = '';
+
   constructor(private authService: AuthService) {}
 
   async ngOnInit() {
     try {
       this.allTodos = await this.authService.getAllTodos();
       console.log(this.allTodos);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async createTodo() {
+    if (this.title) {
+      try {
+        const resp = await this.authService.createTodo(this.title);
+        console.log(resp);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  async deleteTodo(id: number) {
+    try {
+      const resp = await this.authService.deleteTodo(id);
+      console.log(resp);
     } catch (err) {
       console.error(err);
     }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -16,9 +16,24 @@ export class AuthService {
     return lastValueFrom(this.http.post(url, body));
   }
 
+  //Eigentlich in einen anderen Service oder direkt in komponente:
+
   async getAllTodos() {
     const url = environment.baseUrl + '/todos/';
 
     return lastValueFrom(this.http.get(url));
+  }
+
+  async createTodo(title: string) {
+    const url = environment.baseUrl + '/todos/';
+    const body = { title: title };
+
+    return lastValueFrom(this.http.post(url, body));
+  }
+
+  deleteTodo(id: number) {
+    const url = environment.baseUrl + '/todos/' + id + '/';
+
+    return lastValueFrom(this.http.delete(url));
   }
 }
